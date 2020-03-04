8930867a74394bd7d036c741a427dbd46ba49cd9
David Styrbjörn <davidstyrbjorn@gmail.com>
2020-03-04 15:09:30 +0100

    Yo wtf

--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -1,8 +1,38 @@
-#include<iostream>
+#include <GLFW/glfw3.h>
 
-int main() {
+int main(void)
+{
+	GLFWwindow* window;
 
-	std::cout << "Feed him acid and put on rug rats" << std::endl;
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
 
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+		glClearColor(0.4, 0.4, 1.0f, 1.0f);
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 }
