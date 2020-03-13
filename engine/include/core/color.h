384a5bcff48b98c2d4c3c913b15dc29aa748438e
CrispyFruits <hugodahl99@hotmail.com>
2020-03-13 14:40:14 +0100

    colol class

---
+++ engine/include/core/color.h
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+namespace terr {
+
+	class Color {
+	private:	
+
+	public:
+		float r, g, b, a;
+
+		Color();
+		Color(const Color& c) = default;
+		Color(float red, float green, float blue);
+		Color(float red, float green, float blue, float alpha);
+		
+		static Color Red();
+		static Color Green();
+		static Color Blue();
+		static Color GetRandomColor();
+		
+		Color Mix(Color c, const float bias = 0.5);
+	};
+}
