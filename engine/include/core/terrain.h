5d953a86fad0f964cb5d50eb381adf6469da21b2
Hugo Dahl <hugodahl99@hotmail.com>
2020-05-13 19:44:59 +0200

    Revert "Revert "Merge branch 'master' of https://github.com/davidstyrbjorn/terr""
    This reverts commit dfe7c1b6fb9cc98e930cedbbf9545c983440b77d.

--- engine/include/core/terrain.h
+++ engine/include/core/terrain.h
@@ -1,6 +1,8 @@
 #include<vector>
 #include<glm/vec3.hpp>
 
+#include"noise/perlin_noise.h"
+
 #define PI 3.14159265359f
 
 namespace terr {
@@ -15,21 +17,27 @@ namespace terr {
 	private:
 		// Helper methods
 		void GenerateIndiciesBuffer();
+		void UpdateVertexBuffer(); // Uses nodes member as data
 
 	public:
 		Terrain();
 		~Terrain();
 
-		void ConstructTerrain(int _size, float _scale);
+		void ConstructTerrain(int _size, glm::vec<3, int> _scale, uint seed);
 		void RenderTerrain();
 		void UpdateTerrain(float t);
-
-		int size;
-		float scale;
+		
+		// OpenGL related
 		unsigned int vbo, ibo;
 		unsigned int vao;
 		int index_count;
+
+		// Terrain data
+		int size;
+		glm::vec<3, int> scale;
 		std::vector<NodeData> nodes;
 		std::vector<glm::vec3> starting_points;
+		uint seed;
+		PerlinNoise3D perlin_noise;
 	};
 }
